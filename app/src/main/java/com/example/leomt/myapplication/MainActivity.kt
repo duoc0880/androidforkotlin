65eab62d24bf48316a022ac5d1c37ae34e227bb5
duoc0880 <duoc0880@gmail.com>
2018-03-01 19:16:38 +0700

    ! reinit project

--- app/src/main/java/com/example/leomt/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/leomt/myapplication/MainActivity.kt
@@ -2,43 +2,30 @@ package com.example.leomt.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
-
 class MainActivity : AppCompatActivity() {
-    var tv : TextView? = null
-    var btnTrue : Button? = null
-    var btnFalse : Button? = null
 
+    var btn1 : Button? = null
+    var btn2 : Button ?= null
+    var tv : TextView ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
         tv = findViewById(R.id.tv)
-        btnTrue = findViewById(R.id.btnTrue)
-        btnFalse = findViewById(R.id.btnFalse)
 
+        btn1!!.setOnClickListener(){
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show()
+        }
 
-        btnTrue?.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(p0: View?) {
-                val toast = Toast.makeText(this@MainActivity, R.string.correct_toast, Toast.LENGTH_SHORT)
-                toast.setGravity(48,17,17)
-                toast.show()
+        btn2!!.setOnClickListener(){
+            Toast.makeText(this, "Uncorrect", Toast.LENGTH_SHORT).show()
+        }
 
-            }
-        })
-
-        btnFalse?.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(p0: View?) {
-                val toast = Toast.makeText(this@MainActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT)
-                toast.setGravity(48,17,17)
-                toast.show()
-
-
-            }
-        })
     }
 }
+
