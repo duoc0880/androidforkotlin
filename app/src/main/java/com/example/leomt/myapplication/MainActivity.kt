1ca13f80d38d451f655fff57c9f0a56f146a3c78
duoc0880 <duoc0880@gmail.com>
2018-03-01 20:11:12 +0700

    From Button to ImageButton

--- app/src/main/java/com/example/leomt/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/leomt/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.leomt.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,14 +11,16 @@ class MainActivity : AppCompatActivity() {
 
     var btn1 : Button? = null
     var btn2 : Button? = null
-    var btn3 : Button? = null
-    var btn4 : Button? = null
+    var imgbtn1 : ImageButton? = null
+    var imgbtn2 : ImageButton? = null
     var tv : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn1 = findViewById(R.id.btn1)
         btn2 = findViewById(R.id.btn2)
+        imgbtn1 = findViewById(R.id.imgbtn1)
+        imgbtn2 = findViewById(R.id.imgbtn2)
         tv = findViewById(R.id.tv)
 
         btn1!!.setOnClickListener(){
@@ -27,6 +30,14 @@ class MainActivity : AppCompatActivity() {
         btn2!!.setOnClickListener(){
             Toast.makeText(this, "Uncorrect", Toast.LENGTH_SHORT).show()
         }
+        imgbtn1!!.setOnClickListener(){
+
+        }
+        imgbtn2!!.setOnClickListener(){
+
+        }
+
+
     }
 }
 
