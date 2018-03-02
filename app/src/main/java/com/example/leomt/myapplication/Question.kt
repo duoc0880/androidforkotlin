6e1155229bc6af8bebf92e7c9982a012eccdb58b
ADMIN\Admin <duoc0880@gmail.com>
2018-03-02 19:39:09 +0700

    chapter3. custom FrameLayout

---
+++ app/src/main/java/com/example/leomt/myapplication/Question.kt
@@ -0,0 +1,13 @@
+package com.example.leomt.myapplication
+
+import java.security.KeyStore
+
+/**
+ * Created by Admin on 3/2/2018.
+ */
+class Question {
+    val mTextResId : Int = 0
+    val mAnswerTrue: Boolean = true
+
+
+}
