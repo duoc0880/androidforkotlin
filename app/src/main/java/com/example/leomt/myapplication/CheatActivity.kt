207a2d426cfed98002205374b1c2c1c2dafa1f07
duoc0880 <duoc0880@gmail.com>
2018-03-10 11:16:46 +0700

    add class CheatActivity

---
+++ app/src/main/java/com/example/leomt/myapplication/CheatActivity.kt
@@ -0,0 +1,14 @@
+package com.example.leomt.myapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class CheatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.cheat)
+    }
+
+}
