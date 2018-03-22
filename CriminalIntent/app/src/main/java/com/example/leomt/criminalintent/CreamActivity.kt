14f24075d5f805ce6eb39ea87863eb3667dc3a21
duoc0880 <duoc0880@gmail.com>
2018-03-22 20:24:07 +0700

    +add CriminalIntent

---
+++ CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CreamActivity.kt
@@ -0,0 +1,12 @@
+package com.example.leomt.criminalintent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cream)
+    }
+}
