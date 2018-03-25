5310ffbd09949ce589fd3aca3abc4804186af32e
ADMIN\Admin <duoc0880@gmail.com>
2018-03-25 23:16:41 +0700

    +add criminalItent

---
+++ CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/Crime.kt
@@ -0,0 +1,17 @@
+package com.example.leomt.criminalintent
+
+import java.util.*
+
+class Crime {
+
+    var mId : UUID? = null
+    var mTittle : String? = null
+    var mDate : Date? = null
+    var mSlove : Boolean? = false
+
+    constructor( ){
+        mId = UUID.randomUUID()
+        mDate = Date()
+    }
+
+}
