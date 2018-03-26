99c0fcffcf180439baa112ed4cfc926854d642f5
duoc0880 <duoc0880@gmail.com>
2018-03-26 18:41:55 +0700

    +add: CrimeFragment

--- CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CrimeFragment.kt
@@ -2,12 +2,21 @@ package com.example.leomt.criminalintent
 
 import android.app.Fragment
 import android.os.Bundle
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 
 class CrimeFragment : Fragment() {
     var mCrime : Crime? = null
+    var mTitleField : EditText? = null
+    var mDateButton : Button? = null
+    var mSloveCheckBox: CheckBox? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +26,9 @@ class CrimeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         var v : View
         v = inflater!!.inflate(R.layout.fragment_crime, container, false)
+
+        mTitleField = v.findViewById(R.id.crime_title)
+
         return v
     }
 }
