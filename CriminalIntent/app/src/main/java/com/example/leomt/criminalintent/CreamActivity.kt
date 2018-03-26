bb615223c508caa64cb57f71f8abb36b8b7390d9
duoc0880 <duoc0880@gmail.com>
2018-03-26 20:04:22 +0700

    +add: chapter 7 UI Fragment ---> CriminalItent

--- CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CreamActivity.kt
+++ CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CreamActivity.kt
@@ -2,11 +2,22 @@ package com.example.leomt.criminalintent
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 
 class CreamActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cream)
+
+        var fm : FragmentManager = supportFragmentManager
+        var fragment : Fragment = fm.findFragmentById(R.id.fragment_container)
+        if (fragment == null) run {
+            var fragment = CrimeFragment()
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
     }
 }
--- CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CrimeFragment.kt
@@ -2,15 +2,15 @@ package com.example.leomt.criminalintent
 
 import android.app.Fragment
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
+import android.widget.*
 
-class CrimeFragment : Fragment() {
+class CrimeFragment : android.support.v4.app.Fragment()
+{
     var mCrime : Crime? = null
     var mTitleField : EditText? = null
     var mDateButton : Button? = null
@@ -28,6 +28,33 @@ class CrimeFragment : Fragment() {
         v = inflater!!.inflate(R.layout.fragment_crime, container, false)
 
         mTitleField = v.findViewById(R.id.crime_title)
+        mTitleField?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+        mDateButton = v.findViewById(R.id.crime_date)
+        mDateButton?.setText(mCrime?.mDate?.toString())
+        mDateButton?.isEnabled = false
+
+        mSloveCheckBox = v.findViewById(R.id.crime_cb)
+        mSloveCheckBox?.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
+            override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                mCrime?.mSlove = p1
+
+            }
+        })
+
 
         return v
     }
