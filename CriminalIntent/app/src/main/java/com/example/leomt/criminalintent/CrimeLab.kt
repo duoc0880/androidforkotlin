eeda0ee0947140923d35c62594c77149c2b01adb
duoc0880 <duoc0880@gmail.com>
2018-03-29 20:30:28 +0700

    +add chapter 8 RecyclerView

--- CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CrimeLab.kt
+++ CriminalIntent/app/src/main/java/com/example/leomt/criminalintent/CrimeLab.kt
@@ -1,9 +1,30 @@
 package com.example.leomt.criminalintent
 
+import android.content.Context
+
 /**
  * Created by LEOMT on 3/29/18.
  */
 
 class CrimeLab {
-    
+
+    companion object {
+        var sCrimeLab: CrimeLab? = null
+        var mCrimes : List<Crime>? = null
+
+
+        fun CrimeLab(context: Context) : CrimeLab? {
+            if ( sCrimeLab == null ) {
+                sCrimeLab = CrimeLab (context)
+            }
+            return sCrimeLab
+
+        }
+    }
+
+    constructor( context : Context){
+        val mCrimes = ArrayList<Crime>()
+
+        CrimeLab.mCrimes = mCrimes
+    }
 }
